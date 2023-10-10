309684b8502ba756c4156472d8b32e89780171a3
TZProgrammer <thomaszeger@gmail.com>
2023-10-10 10:39:03 -0400

    Add live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local builtin = require('telescope.builtin')
 
+
 -- File search
 vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden = true}) end, {})
-vim.keymap.set('n', '<leader>fd', function() builtin.find_files({hidden = true, search_dirs = {vim.fn.input("Directory > ")}}) end, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fd', function() builtin.find_files({hidden = true, search_dirs = {vim.fn.input("Directory > ")}}) end, {})
 
 
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
@@ -16,7 +17,19 @@ end)
 vim.keymap.set('n', '<leader>gg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > "), search_dirs = builtin.git_files()})
 end)
-vim.keymap.set('n', '<leader>gt', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>gd', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") , search_dirs = {vim.fn.input("Directory > ")}})
+end)
+vim.keymap.set('n', '<leader>gt', builtin.grep_string, {})
+
+
+-- Live Grep search
+vim.keymap.set('n', '<leader>lgf', function()
+	builtin.live_grep({})
+end)
+vim.keymap.set('n', '<leader>lgg', function()
+	builtin.live_grep({search_dirs = builtin.git_files()})
+end)
+vim.keymap.set('n', '<leader>lgd', function()
+	builtin.live_grep({search_dirs = {vim.fn.input("Directory > ")}})
 end)
