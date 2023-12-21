ce7067fa9d2d1f06847b97406a2e83155c05ec56
TZProgrammer <thomaszeger@gmail.com>
2023-12-21 10:46:32 -0500

    Fix buffer unaligned issue

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,6 +31,16 @@ require('mason-lspconfig').setup({
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
+    clangd = function()
+        local cmp_nvim_lsp = require "cmp_nvim_lsp"
+        require("lspconfig").clangd.setup {
+            capabilities = cmp_nvim_lsp.default_capabilities(),
+            cmd = {
+                "clangd",
+                "--offset-encoding=utf-16",
+            },
+        }
+    end,
   }
 })
 
