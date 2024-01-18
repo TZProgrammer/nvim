0b17912bdffe136c15ebf2504285b14ec9e1278c
TZProgrammer <thomaszeger@gmail.com>
2024-01-18 14:30:34 -0500

    Add commit amend remap and modify remaps for git

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -13,18 +13,19 @@ autocmd("BufWinEnter", {
 
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "<leader>gps", function()
+        vim.keymap.set("n", "<leader>ps", function()
             vim.cmd.Git('push')
         end, opts)
 
         -- rebase always
-        vim.keymap.set("n", "<leader>gpl", function()
+        vim.keymap.set("n", "<leader>pl", function()
             vim.cmd.Git({'pull',  '--rebase'})
         end, opts)
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
         -- needed if i did not set the branch up correctly
         vim.keymap.set("n", "<leader>t", ":G push -u origin ", opts);
-        vim.keymap.set("n", "<leader>gc", ":G commit -m \"\"<Left>", opts);
+        vim.keymap.set("n", "<leader>cm", ":G commit -m \"\"<Left>", opts);
+        vim.keymap.set("n", "<leader>ca", ":G commit --amend<CR>", opts);
     end,
 })
